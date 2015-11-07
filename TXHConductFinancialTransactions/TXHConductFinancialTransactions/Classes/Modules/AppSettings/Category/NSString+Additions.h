655dba094f963bd30a7392f0fcd1f5bba1244b8b
2252819957 <wei.wutao@rongyu100.com>
2015-11-07 12:47:31 +0800

    金钱格式化

---
+++ TXHConductFinancialTransactions/TXHConductFinancialTransactions/Classes/Modules/AppSettings/Category/NSString+Additions.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Additions.h
+//  TXHConductFinancialTransactions
+//
+//  Created by wwt on 15/11/7.
+//  Copyright (c) 2015年 rongyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+//字符串金钱格式化
+-(NSString *)changeFormatwithMoneyAmount;
+
+@end

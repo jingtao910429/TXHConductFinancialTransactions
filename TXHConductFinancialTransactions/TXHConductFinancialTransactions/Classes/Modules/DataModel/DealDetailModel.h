5f5f8f456fc3d2d848a8bd6c912b2651cab48b02
2252819957 <wei.wutao@rongyu100.com>
2015-11-07 12:58:09 +0800

    交易记录添加

---
+++ TXHConductFinancialTransactions/TXHConductFinancialTransactions/Classes/Modules/DataModel/DealDetailModel.h
@@ -0,0 +1,13 @@
+//
+//  DealDetailModel.h
+//  TXHConductFinancialTransactions
+//
+//  Created by wwt on 15/11/7.
+//  Copyright (c) 2015年 rongyu. All rights reserved.
+//
+
+#import "CommonModel.h"
+
+@interface DealDetailModel : CommonModel
+
+@end

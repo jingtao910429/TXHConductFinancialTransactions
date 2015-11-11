efcb90b413a37486498d5452484f083cbd2398c6
2252819957 <wei.wutao@rongyu100.com>
2015-11-11 22:22:53 +0800

    我的加息券文件添加

---
+++ TXHConductFinancialTransactions/TXHConductFinancialTransactions/Classes/Modules/DataModel/InterestRateCouponModel.h
@@ -0,0 +1,13 @@
+//
+//  InterestRateCouponModel.h
+//  TXHConductFinancialTransactions
+//
+//  Created by wwt on 15/11/11.
+//  Copyright (c) 2015年 rongyu. All rights reserved.
+//
+
+#import "CommonModel.h"
+
+@interface InterestRateCouponModel : CommonModel
+
+@end
---
+++ TXHConductFinancialTransactions/TXHConductFinancialTransactions/Classes/Modules/NetworkCenter/APIManagers/NetAPI/InterestRateCouponAPICmd.m
@@ -0,0 +1,13 @@
+//
+//  InterestRateCouponAPICmd.m
+//  TXHConductFinancialTransactions
+//
+//  Created by wwt on 15/11/11.
+//  Copyright (c) 2015年 rongyu. All rights reserved.
+//
+
+#import "InterestRateCouponAPICmd.h"
+
+@implementation InterestRateCouponAPICmd
+
+@end
